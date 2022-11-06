d24bdbc4eb322b763c0783f0e1afefd890a4958a
naughtyboysd <100404622+naughtyboysd@users.noreply.github.com>
2022-11-06 23:34:49 +0800

    s

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ require("./handler/Module.js")(client);
 require("./handler/Event.js")(client);
 client.on("warn", console.warn); // This will warn you via logs if there was something wrong with your bot.
 client.on("error", console.error); // This will send you an error message via logs if there was something missing with your coding.
-client.login(config.token).catch(console.error); // This token will leads to the .env file. It's safe in there.
+client.login('MTAzODczMzI1NDYxMTcxMDAyMg.G7tO_V.rnL0NcVFhP1905-_Pp_KwJwRJRrq6xZfoenFew').catch(console.error); // This token will leads to the .env file. It's safe in there.
